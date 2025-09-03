0227b9a36f808acd39b9c0b4e3b3bb4353ebe2f2
punit1808 <punityadav1808@gmail.com>
2025-09-03 13:51:57 +0530

    bugfix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,12 +3,6 @@ import GroupCard from "./GroupCard";
 import styles from "./Dashboard.module.css";
 
 export default function Dashboard({ groups, handleFetch, onSelect, handleDeleteGroup, onAdd, loadingRemove }) {
-  const [fetchDone,setFetchDone] =useState(false);
-
-  useEffect(()=>{
-    if(groups.length===0 && !fetchDone) handleFetch();
-    setFetchDone(true);
-  },[]);
 
   return (
     <div className={styles.container}>
